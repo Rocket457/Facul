1ec0b838e53d12af619242bf2678af9a79c1b4ce
Rocket457 <71684147+Rocket457@users.noreply.github.com>
2023-04-11 21:50:56 -0300

    ex varios

--- Ex05/Readme.txt
+++ Ex05/Readme.txt
@@ -1,4 +1,6 @@
-5-  Considere a necessidade de desenvolver um algoritmo que calcule e apresente ao usuário o valor do volume V de uma esfera de raio R. Onde PI é igual 3.14159.
+5-  Considere a necessidade de desenvolver um algoritmo que calcule e 
+apresente ao usuário o valor do volume V de uma esfera de raio R. Onde 
+PI é igual 3.14159.
 
 Formula v=4*pi*r^3/3
 
--- Ex06/Ex06.c
+++ Ex06/Ex06.c
@@ -1,3 +1,18 @@
+#include <stdio.h>
+#include <math.h>
 void main (){
-    
+    float C;
+    float J;
+    float M;
+    float F;
+    printf("informe o Capital inicial:\n");
+    scanf("%f",&C);
+    printf("informe a taxa de juros:\n");
+    scanf("%f",&J);
+    printf("informe qual sera o periodo de aplicacao em meses:\n");
+    scanf("%f",&M);
+    F = pow((1 + J/100), M);
+    F = F * C;
+    printf("o montante final sera:%f ",F);
+
 }
