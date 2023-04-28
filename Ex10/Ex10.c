f72e893db3629b37227da8d92c1a9c426989d065
Gabriel Moreira <130494619+Bzrdgael@users.noreply.github.com>
2023-04-28 20:27:00 -0300

    Update Ex10.c

--- Ex10/Ex10.c
+++ Ex10/Ex10.c
@@ -1,3 +1,15 @@
-int main(){
-    
+#include<stdio.h>
+#include<math.h>
+int main (){
+    float P1, P2, dividido;
+    printf("Valor do P1: ");
+    scanf("%f", &P1);
+    printf("Valor do P2: ");
+    scanf("%f", &P2);
+         P1 = pow(P1, 2);
+         P2 =pow(P2, 2);
+         dividido = (P1/P2);
+     printf("Resultado = %f\n", dividido);
+
+   
 }
